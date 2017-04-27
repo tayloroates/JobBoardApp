832789970799fd6badcdca6c793fdea96f945f3c
anthoflip101 <anthonygoodman45@gmail.com>
2017-04-27 13:00:43 -0500

    Changed Again

--- src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
+++ src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
@@ -2,6 +2,16 @@ namespace JobBoardApp.Controllers {
 
     export class HomeController {
         public message = 'Hello from the home page!';
+
+    }
+    export class JobController {
+        public jobs;
+
+        constructor($http: ng.IHttpService) {
+            $http.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=145417&t.k=jpMvfETM5pS&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0').then((results) => {
+                this.jobs = results.data;
+            });
+        }
     }
 
 

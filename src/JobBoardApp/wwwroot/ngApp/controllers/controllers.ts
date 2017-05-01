7920f5284c4c8280c1e1dea3a5815116c99114f4
anthoflip101 <anthonygoodman45@gmail.com>
2017-05-01 09:14:45 -0500

    COMMITED

--- src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
+++ src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
@@ -2,13 +2,22 @@ namespace JobBoardApp.Controllers {
 
     export class HomeController {
         public message = 'Hello from the home page!';
+    
+        public jobs = "http://api.glassdoor.com/api/api.htm?t.p=145417&t.k=jpMvfETM5pS&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1";
 
+        
+        constructor(private $http: ng.IHttpService) {
+            this.$http.get('/api/jobs')
+                .then((response) => {
+                    this.jobs = response.data;
+                });
+        }
     }
     export class JobController {
         public jobs;
 
         constructor($http: ng.IHttpService) {
-            $http.get('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=145417&t.k=jpMvfETM5pS&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0').then((results) => {
+            $http.get('http://api.glassdoor.com/api/api.htm?t.p=145417&t.k=jpMvfETM5pS&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1').then((results) => {
                 this.jobs = results.data;
             });
         }

a518136c9c8a96994fa72a6a1b0ea1da5ec15c46
anthoflip101 <anthonygoodman45@gmail.com>
2017-05-04 13:41:11 -0500

    Committed

--- src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
+++ src/JobBoardApp/wwwroot/ngApp/controllers/controllers.ts
@@ -2,11 +2,27 @@ namespace JobBoardApp.Controllers {
 
     export class HomeController {
         public message = 'Hello from the home page!';
+        public JobResource;
         public jobs;
         public jobsUrl = "http://api.glassdoor.com/api/api.htm?t.p=145417&t.k=jpMvfETM5pS&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1";
 
-        
-        constructor(private $http: ng.IHttpService) {
+        public getJobs() {
+            this.jobs = this.JobResource.query();
+
+        }
+
+        public save() {
+            this.JobResource.save(this.jobs).$promise.then(() => {
+                this.jobs = null;
+                this.getJobs();
+            });
+        }
+
+
+        constructor(private $http: ng.IHttpService,) {
+            //this.JobResource = $resource(this.jobUrl);
+            //this.getJobs();
+
             this.$http.get(this.jobsUrl)
                 .then((response) => {
                     this.jobs = response.data;
@@ -14,10 +30,23 @@ namespace JobBoardApp.Controllers {
         }
     }
     export class JobController {
+        public jobTitleResource;
         public jobTitle;
         public jobTitleUrl = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=145417&t.k=jpMvfETM5pS&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0";
 
-        constructor(private $http: ng.IHttpService) {
+        public getJobTitle() {
+            this.jobTitle = this.jobTitleResource.query();
+
+        }
+
+        public save() {
+            this.jobTitleResource.save(this.jobTitle).$promise.then(() => {
+                this.jobTitle = null;
+                this.getJobTitle();
+            });
+        }
+        constructor(private $resource: ng.resource.IResourceService, private $http: ng.IHttpService) {
+            //t/*his.jobTitleResource = $resource(this.jobTitleUrl);*/
             $http.get(this.jobTitleUrl).then((results) => {
                 this.jobTitle = results.data;
             });
--- src/JobBoardApp/wwwroot/ngApp/services/services.ts
+++ src/JobBoardApp/wwwroot/ngApp/services/services.ts
@@ -1,26 +1,26 @@
 namespace JobBoardApp.Services {
-    //export class JobService {
-    //    public jobResource;
+    export class JobService {
+        public jobResource;
 
-    //    public getJobs() {
-    //        return this.jobResource.query();
-    //    }
+        public getJobs() {
+            return this.jobResource.query();
+        }
 
-    //    public save(job) {
-    //        return this.jobResource.save(job).$promise;
-    //    }
-    //    public deleteJob(id: number) {
-    //        return this.jobResource.delete({ id: id }).$promise;
-    //    }
-    //    public getJob(id) {
-    //        return this.jobResource.get({ id: id });
-    //    }
-    //    constructor($resource: ng.resource.IResourceService) {
-    //        this.jobResource = $resource('http://api.glassdoor.com/api/api.htm?t.p=145417&t.k=jpMvfETM5pS&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1');
-    //    }
+        public save(job) {
+            return this.jobResource.save(job).$promise;
+        }
+        public deleteJob(id: number) {
+            return this.jobResource.delete({ id: id }).$promise;
+        }
+        public getJob(id) {
+            return this.jobResource.get({ id: id });
+        }
+        constructor($resource: ng.resource.IResourceService) {
+            this.jobResource = $resource('http://api.glassdoor.com/api/api.htm?t.p=145417&t.k=jpMvfETM5pS&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1');
+        }
 
-    //}
-    //angular.module('JobBoardApp').service('JobService', JobService);
+    }
+    angular.module('JobBoardApp').service('JobService', JobService);
 
 }
 
